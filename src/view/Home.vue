857014492ddf81dbfb6d7779daf0a1b302eeffa0
Sherif <saykeed@gmail.com>
2022-06-20 11:19:40 +0100

    initial commit

---
+++ src/view/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="home">
+        i am the home page
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+@import "../assets/scss/variable.scss";
+    
+</style>
